7feef26ba5ab116af269e93cab798630e80f8cd7
laumansillaa <laumansillaa@hotmail.com>
2023-06-06 21:15:44 -0300

    -

--- api/index.js
+++ api/index.js
@@ -9,28 +9,28 @@ const task = cron.schedule('*/14 * * * *', () => {
   });
   task.start();
 
-db.sync({force: false}).then(async () => {
+db.sync({force: true}).then(async () => {
 
     try {
-        // console.log('Data', data.length)
-        // for (var i = 0; i<data.length; i++) {
-        //     await Producto.create({
-        //         nombre: data[i].nombre,
-        //         descripcion: data[i].descripcion,
-        //         img: data[i].img,
-        //         catalogo: data[i].catalogo,
-        //         categoria: data[i].categoria,
-        //         prov: data[i].prov
-        //     }).then((response) => {
-        //         console.log('Nuevo Producto')
-        //     })
-        // }
+        console.log('Data', data.length)
+        for (var i = 0; i<data.length; i++) {
+            await Producto.create({
+                nombre: data[i].nombre,
+                descripcion: data[i].descripcion,
+                img: data[i].img,
+                catalogo: data[i].catalogo,
+                categoria: data[i].categoria,
+                prov: data[i].prov
+            }).then((response) => {
+                console.log('Nuevo Producto')
+            })
+        }
 
-        // await Admin.create({
-        //     email: "sonda@admin.com",
-        //     password: "sonda",
-        //     isAdmin: true
-        // })
+        await Admin.create({
+            email: "sonda@admin.com",
+            password: "sonda",
+            isAdmin: true
+        })
 
     } catch (error) {
         console.log(error)
